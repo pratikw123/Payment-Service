297e65253e99597e27c1e1b06f3db7aeb73b5507
pratikw123 <pratikwadaskar2022@gmail.com>
2026-05-17 15:13:04 +0530

    Create ApiResponse.java

---
+++ src/main/java/com/pratik/paymentservice/dto/ApiResponse.java
@@ -0,0 +1,21 @@
+package com.pratik.paymentservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/** Generic API response wrapper for success/error messages */
+@Data
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private boolean success;
+    private String message;
+    private T data;
+
+    public static <T> ApiResponse<T> success(String message, T data) {
+        return new ApiResponse<>(true, message, data);
+    }
+
+    public static <T> ApiResponse<T> error(String message) {
+        return new ApiResponse<>(false, message, null);
+    }
+}
